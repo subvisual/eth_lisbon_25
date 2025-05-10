627534e2ed174687d9b627a80297b826b93390b4
Pedro Ferreira <pferreira59@gmail.com>
2025-05-10 17:11:23 +0100

    wip

--- lib/providers.tsx
+++ lib/providers.tsx
@@ -1,58 +1,18 @@
 "use client";
 
-import React from "react";
-import { DynamicContextProvider } from "@dynamic-labs/sdk-react-core";
-import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
-import { chiadoNetwork, sepoliaNetwork } from "./evmNetworks";
-
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { WagmiProvider } from "wagmi";
 import { config as wagmiConfig } from "./wagmi";
+import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
 
 const queryClient = new QueryClient();
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  // ensure our API key is defined as a string
-  const dynamicApiKey = process.env.NEXT_PUBLIC_DYNAMIC_API_KEY;
-  if (!dynamicApiKey) {
-    throw new Error("Missing NEXT_PUBLIC_DYNAMIC_API_KEY environment variable");
-  }
-
   return (
-    <DynamicContextProvider
-      theme="light"
-      settings={{
-        environmentId: dynamicApiKey,
-        walletConnectors: [EthereumWalletConnectors],
-        overrides: {
-          evmNetworks: (dashboardNetworks) => {
-            const gnosisMainnet = dashboardNetworks.find(
-              (n) => n.chainId === 100
-            );
-            const others = dashboardNetworks.filter((n) => n.chainId !== 100);
-            const hasChiado = dashboardNetworks.some(
-              (n) => n.chainId === chiadoNetwork.chainId
-            );
-
-            const hasSepolia = dashboardNetworks.some(
-              (n) => n.chainId === sepoliaNetwork.chainId
-            );
-
-            return [
-              ...(gnosisMainnet ? [gnosisMainnet] : []),
-              ...others,
-              ...(hasChiado ? [] : [chiadoNetwork]),
-              ...(hasSepolia ? [] : [sepoliaNetwork]),
-            ];
-          },
-        },
-      }}
-    >
-      <WagmiProvider config={wagmiConfig}>
-        <QueryClientProvider client={queryClient}>
-          {children}
-        </QueryClientProvider>
-      </WagmiProvider>
-    </DynamicContextProvider>
+    <WagmiProvider config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider> {children} </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
   );
 }
