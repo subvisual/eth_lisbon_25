4dbda29ce6d8c93fda1d6db6f3d36387ac352e2b
Luís Torres <luistorres1792@gmail.com>
2025-05-10 17:28:02 +0100

    fix lock and page

--- app/bridging/page.tsx
+++ app/bridging/page.tsx
@@ -21,18 +21,21 @@ export default function BridgeAndSwapWidget() {
         {
           address: "0x2a22f9c3b484c3629090FeED35F17Ff8F88f76F0",
           symbol: "USDC",
+          name: "USDC",
           decimals: 6,
           chainId: 100,
         },
         {
           address: "0xcB444e90D8198415266c6a2724b7900fb12FC56E",
           symbol: "EURe",
+          name: "EURe",
           decimals: 18,
           chainId: 100,
         },
         {
           address: "0x5Cb9073902F2035222B9749F8fB0c9BFe5527108",
           symbol: "EURe",
+          name: "EURe",
           decimals: 18,
           chainId: 100,
         },
